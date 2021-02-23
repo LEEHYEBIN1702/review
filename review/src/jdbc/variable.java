e0e79d3746b2769f0efecb64c02795e66718bbb5
leehyebin <qls1702@hanmail.net>
2021-02-23 18:04:11 +0900

    2021 02 23 자바 자바스크립트 차이 공부 필기 참조

---
+++ review/src/jdbc/variable.java
@@ -0,0 +1,15 @@
+package jdbc;
+
+public class variable {
+	public static void main(String[] args) {
+	       
+	            {
+	        int b =5;
+	            }
+	        int b =10;
+	        for (int i=0; i<10; i++) {
+	        	
+	        }
+	        System.out.println(b);
+	}
+}
